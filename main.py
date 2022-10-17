7fac3d1fa557886af9b6a328ac80a2e21881498b
Sergio Basurto <35089326+sebasurto@users.noreply.github.com>
2022-10-17 22:48:50 +0000

    más estético

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import time
 import taskaa as tsk
 
 entradas = ["100","500","1000","5000","10000"]
-for i in entradas:
-  data =tsk.loadFile (i +".csv")
-  data =tsk.convertListToInt (data)
-  inicio = time.time()
-  tsk.mergeSort(data,0,len(data)-1)
-  fin = time.time()
-  print("Tiempo con "+i+ " datos:\n",(fin-inicio))
+for n in range(5):
+  print ("\nPrueba #",n+1," \n")
+  for i in entradas:
+    data =tsk.loadFile (i +".csv")
+    data =tsk.convertListToInt (data)
+    inicio = time.time()
+    tsk.mergeSort(data,0,len(data)-1)
+    fin = time.time()
+    print("Tiempo con "+i+ " datos:\n",(fin-inicio)%.7)
 
 #print (data)
